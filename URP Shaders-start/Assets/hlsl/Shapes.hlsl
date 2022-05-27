3762280a81cfea2db2f30b657ccc3d086bacfb8f
Christophe Savard <christophe_savard@hotmail.ca>
2022-05-27 12:03:06 -0400

    Part 13

---
+++ URP Shaders-start/Assets/hlsl/Shapes.hlsl
@@ -0,0 +1,37 @@
+﻿#ifndef SHAPES
+#define SHAPES
+
+void circle_float(float2 pos, float2 center, float radius, out float result)
+{
+    float len = length(pos - center);
+    float halfWidth = radius / 20;
+    float edge = halfWidth / 10;
+    result = smoothstep(radius - halfWidth - edge, radius - halfWidth,        len)
+           - smoothstep(radius + halfWidth,        radius + halfWidth + edge, len);
+}
+
+void onLine_float(float a, float b, float width, out float result)
+{
+    float halfLine = width / 2;
+    float edge = halfLine / 10;
+    result = smoothstep(a - halfLine - edge, a - halfLine, b)
+           - smoothstep(a + halfLine, a + halfLine + edge, b);
+}
+
+void cross_float(float2 pos, float2 center, float length, out float result)
+{
+    pos -= center;
+    if (pos.x > length || pos.x < -length || pos.y > length || pos.y < -length)
+    {
+        result = 0;
+        return;
+    }
+
+    float horizontal;
+    float vertical;
+    float width = length / 10;
+    onLine_float(pos.x, 0, width, horizontal);
+    onLine_float(pos.y, 0, width, vertical);
+    result = saturate(horizontal + vertical);
+}
+#endif
